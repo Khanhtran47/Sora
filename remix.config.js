f09e602ab71c9eb838aed6359a20d4f23c297a19
Restyled.io <commits@restyled.io>
2022-07-10 18:19:27 +0000

    Restyled by clang-format

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,10 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-  serverBuildDirectory: 'api/_build',
-  ignoredRouteFiles: ['.*'],
-  serverDependenciesToBundle: ['swiper'],
+  appDirectory : 'app',
+  assetsBuildDirectory : 'public/build',
+  publicPath : '/build/',
+  serverBuildDirectory : 'api/_build',
+  ignoredRouteFiles : [ '.*' ],
+  serverDependenciesToBundle : [ 'swiper' ],
 };
