c753ec28b23b47edf814eb26aa1e19ec1acee310
Duc Khanh <duc_khanh.tran@insa-cvl.fr>
2022-07-26 22:22:55 +0200

    test with react 17

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,4 @@
-import { hydrateRoot } from 'react-dom/client';
+import { hydrate } from 'react-dom';
 import { RemixBrowser } from '@remix-run/react';
 
-hydrateRoot(document, <RemixBrowser />);
+hydrate(<RemixBrowser />, document);
