ae60aedbbad8ac2eedddfe942d6c60aa706080a3
Cong Tuan Le <congtuanle23@gmail.com>
2022-09-08 10:04:47 +0200

    fix: update RowItem style
    - maxWith 1400px
    - TMDb instead of IMDb :v
    - increase row gap to 2rem
    - update box shadow for dark mode (what is grey grey, idk)

--- app/src/components/media/item/RowItem.tsx
+++ app/src/components/media/item/RowItem.tsx
@@ -8,25 +8,33 @@ interface IRowItem {
 }
 const RowItem = ({ item }: IRowItem) => {
   const isXs = useMediaQuery(425, 'max');
+  const isSm = useMediaQuery(650, 'max');
   const { isDark } = useTheme();
 
   const href = (item.mediaType === 'movie' ? '/movies/' : '/tv-shows/') + item.id;
   const year = item.releaseDate ? new Date(item.releaseDate).getFullYear() : undefined;
-  const overview = item.overview && isXs ? `${item.overview.substring(0, 100)}...` : item.overview;
+
+  let overview = item.overview ?? '';
+  if (isXs) {
+    overview = `${overview.substring(0, 100)}...`;
+  } else if (isSm) {
+    overview = `${overview.substring(0, 200)}...`;
+  }
 
   return (
     <Grid.Container
-      gap={1}
+      gap={2}
       alignItems="flex-start"
       css={{
         padding: '0',
-        marginBottom: '1.5rem',
-        boxShadow: `${isDark ? 'rgba(255, 255, 255, 0.35)' : 'rgba(0, 0, 0, 0.35)'} 0px 5px 0.5rem`,
+        marginBottom: '2rem',
+        boxShadow: `${isDark ? 'rgba(50, 50, 50, 0.5)' : 'rgba(0, 0, 0, 0.35)'} 0 3px 0.5rem`,
         borderRadius: '0.5rem',
         maxHeight: '15rem',
+        maxWidth: '1400px',
       }}
     >
-      <Grid xl={1} lg={2} sm={3} xs={4} css={{ padding: '$0' }}>
+      <Grid xl={1} lg={1.5} md={2} sm={2.5} xs={4} css={{ padding: '$0' }}>
         <Link to={href}>
           <Image
             showSkeleton
@@ -36,7 +44,7 @@ const RowItem = ({ item }: IRowItem) => {
           />
         </Link>
       </Grid>
-      <Grid xl={11} lg={10} sm={9} xs={8}>
+      <Grid xl={11} lg={10.5} md={10} sm={9.5} xs={8}>
         <Grid.Container css={{ marginRight: `${isXs ? '0.15rem' : '1rem'}` }}>
           <Grid xs={12}>
             <Link to={href}>
@@ -59,13 +67,13 @@ const RowItem = ({ item }: IRowItem) => {
               weight="bold"
               size="$xs"
               css={{
-                backgroundColor: '#dba314',
+                backgroundColor: '#3ec2c2',
                 borderRadius: '$xs',
                 padding: '0 0.25rem 0 0.25rem',
                 marginRight: '0.5rem',
               }}
             >
-              IMDb
+              TMDb
             </Text>
             <Text size="$sm" weight="bold">
               {item.voteAverage}
