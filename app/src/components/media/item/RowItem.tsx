cc28b56b601647ec35d980317fb0f4b8d371920f
Cong Tuan Le <70543626+ctuanle@users.noreply.github.com>
2022-09-18 16:29:23 +0200

    feat:add credits tab in people detail (#156)

--- app/src/components/media/item/RowItem.tsx
+++ app/src/components/media/item/RowItem.tsx
@@ -1,39 +1,53 @@
+/* eslint-disable no-nested-ternary */
 import { Grid, Image, Text, useTheme } from '@nextui-org/react';
 import { Link } from '@remix-run/react';
-import useMediaQuery from '~/hooks/useMediaQuery';
 import { IMedia } from '~/services/tmdb/tmdb.types';
 
 interface IRowItem {
   item: IMedia;
+  containerWidth: number;
+  simplified?: boolean;
 }
-const RowItem = ({ item }: IRowItem) => {
-  const isXs = useMediaQuery(425, 'max');
-  const isSm = useMediaQuery(650, 'max');
+
+const RowItem = ({ item, containerWidth, simplified }: IRowItem) => {
+  const isHs = containerWidth <= 360; // hyper small :v
+  const isXs = containerWidth <= 425; // extra small
+  const isSm = containerWidth <= 650;
+
   const { isDark } = useTheme();
 
   const href = (item.mediaType === 'movie' ? '/movies/' : '/tv-shows/') + item.id;
   const year = item.releaseDate ? new Date(item.releaseDate).getFullYear() : undefined;
 
-  let overview = item.overview ?? '';
-  if (isXs) {
-    overview = `${overview.substring(0, 100)}...`;
-  } else if (isSm) {
-    overview = `${overview.substring(0, 200)}...`;
+  const outerContainerCss = {
+    padding: '0',
+    marginBottom: '2rem',
+    boxShadow: `${isDark ? 'rgba(50, 50, 50, 0.5)' : 'rgba(0, 0, 0, 0.35)'} 0 3px 0.5rem`,
+    borderRadius: '0.5rem',
+    maxHeight: '15rem',
+    maxWidth: '1400px',
+  };
+
+  if (simplified) {
+    return (
+      <Grid.Container gap={2} alignItems="flex-start" css={outerContainerCss}>
+        <Grid xs={2} sm={1.5}>
+          <Text>{year ?? '-'}</Text>
+        </Grid>
+        <Grid xs={1.5} sm={1}>
+          o
+        </Grid>
+        <Grid xs={8.5} sm={9.5}>
+          <Link to={href}>
+            <Text weight="bold">{item.title}</Text>
+          </Link>
+        </Grid>
+      </Grid.Container>
+    );
   }
 
   return (
-    <Grid.Container
-      gap={2}
-      alignItems="flex-start"
-      css={{
-        padding: '0',
-        marginBottom: '2rem',
-        boxShadow: `${isDark ? 'rgba(50, 50, 50, 0.5)' : 'rgba(0, 0, 0, 0.35)'} 0 3px 0.5rem`,
-        borderRadius: '0.5rem',
-        maxHeight: '15rem',
-        maxWidth: '1400px',
-      }}
-    >
+    <Grid.Container gap={2} alignItems="flex-start" css={outerContainerCss}>
       <Grid xl={1} lg={1.5} md={2} sm={2.5} xs={4} css={{ padding: '$0' }}>
         <Link to={href}>
           <Image
@@ -55,14 +69,28 @@ const RowItem = ({ item }: IRowItem) => {
           </Grid>
 
           <Grid xs={12}>
-            <Text h6>{`${item.originalTitle} (${year})`}</Text>
+            <Text
+              h6
+              css={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+            >{`${item.originalTitle} (${year})`}</Text>
           </Grid>
           <Grid xs={12}>
-            <Text size="$sm" css={{ textAlign: 'justify' }}>
-              {overview}
+            <Text
+              size="$sm"
+              css={{
+                textAlign: 'justify',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'normal',
+                display: '-webkit-box',
+                '-webkit-line-clamp': `${isHs ? 2 : isXs ? 3 : isSm ? 4 : 5}`,
+                '-webkit-box-orient': 'vertical',
+              }}
+            >
+              {item.overview}
             </Text>
           </Grid>
-          <Grid xs={6} css={{ alignItems: 'center' }}>
+          <Grid xs={6} css={{ alignItems: 'center', marginTop: '0.1rem' }}>
             <Text
               weight="bold"
               size="$xs"
