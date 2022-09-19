088f5966ac0a26fa085a0df51dc3b42298cb9247
Cong Tuan Le <congtuanle23@gmail.com>
2022-09-20 00:36:42 +0200

    subtitles

--- app/routes/api/subtitle.ts
+++ app/routes/api/subtitle.ts
@@ -1,12 +1,19 @@
-// import { LoaderFunction, json } from '@remix-run/node';
-// import { convert } from 'subtitle-converter';
+import { LoaderFunction, json, Response } from '@remix-run/node';
+// @ts-expect-error: no type
+import { convert } from 'subtitle-converter';
+import axios from 'axios';
 
-// export const loader: LoaderFunction = async ({ request }) => {
-//   const url = new URL(request.url);
-//   const path = url.searchParams.get('url');
-//   if (!path || typeof path !== 'string') throw new Response('Invalid request', { status: 400 });
-//   const response = (await fetch(encodeURI(path))).json();
-//   const { subtitle } = convert(response, '.vtt');
-//   if (!subtitle) throw new Response('Cannot convert', { status: 400 });
-//   return json(subtitle);
-// };
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const path = url.searchParams.get('url');
+  if (!path || typeof path !== 'string') throw new Response('Invalid request', { status: 400 });
+
+  const response = await axios.get(encodeURI(path));
+  // const response = (await fetch(encodeURI(path))).arrayBuffer();
+  const { subtitle } = convert(response.data, '.vtt');
+  if (!subtitle) throw new Response('Cannot convert', { status: 400 });
+  // return json(subtitle);
+  return new Response(subtitle, {
+    headers: { 'content-type': 'text/vtt' },
+  });
+};
