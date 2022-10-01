383afc5066b0fe09d53e87a815fab5f4d2d41d72
Restyled.io <commits@restyled.io>
2022-10-01 20:22:45 +0000

    Restyled by clang-format

--- app/src/assets/icons/StopIcon.js
+++ app/src/assets/icons/StopIcon.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 
 const Bold = ({ color }) => (
   <path
-    d="M5 3C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5Z"
-    fill={color}
+d = "M5 3C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5Z"
+    fill={
+  color}
   />
 );
 
@@ -17,16 +18,16 @@ const Light = ({ color }) => (
   />
 );
 
-const Stop = ({
-  fill = 'currentColor',
-  filled = false,
-  size = 0,
-  height = 0,
-  width = 0,
-  ...props
-}) => {
-  switch (filled) {
-    case false:
+    const Stop = ({
+      fill = 'currentColor',
+      filled = false,
+      size = 0,
+      height = 0,
+      width = 0,
+      ...props
+    }) => {
+      switch (filled) {
+      case false:
       return (
         <svg
           width={size || width || 24}
@@ -34,10 +35,11 @@ const Stop = ({
           viewBox="0 0 24 24"
           {...props}
         >
-          <Light color={fill} />
+          <Light color={
+      fill} />
         </svg>
       );
-    default:
+      default:
       return (
         <svg
           width={size || width || 24}
@@ -45,29 +47,30 @@ const Stop = ({
           viewBox="0 0 24 24"
           {...props}
         >
-          <Bold color={fill} />;
+          <Bold color={
+      fill} />;
         </svg>
       );
-  }
-};
+      }
+    };
 
-Stop.displayName = 'StopIcon';
+    Stop.displayName = 'StopIcon';
 
-Bold.propTypes = {
-  color: PropTypes.string,
-};
+    Bold.propTypes = {
+      color : PropTypes.string,
+    };
 
-Light.propTypes = {
-  color: PropTypes.string,
-};
+    Light.propTypes = {
+      color : PropTypes.string,
+    };
 
-Stop.propTypes = {
-  fill: PropTypes.string,
-  filled: PropTypes.bool,
-  size: PropTypes.number,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
+    Stop.propTypes = {
+      fill : PropTypes.string,
+      filled : PropTypes.bool,
+      size : PropTypes.number,
+      width : PropTypes.number,
+      height : PropTypes.number,
+      strokeWidth : PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
+    };
 
-export default Stop;
+    export default Stop;
