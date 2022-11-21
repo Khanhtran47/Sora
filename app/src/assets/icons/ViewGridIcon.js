f2518fb7aa65a14f19be044ca65de9e6f968e11d
Restyled.io <commits@restyled.io>
2022-11-21 02:33:41 +0000

    Restyled by prettier

--- app/src/assets/icons/ViewGridIcon.js
+++ app/src/assets/icons/ViewGridIcon.js
@@ -6,43 +6,57 @@ import Svg from '../../components/styles/Svg.styles';
 const Bold = ({ color, strokeWidth }) => (
   <>
     <rect
-x = "6"
-y = "6"
-width = "36"
-height = "36"
-rx = "3"
-fill = {color} stroke = {color} strokeWidth = {strokeWidth} strokeLinejoin =
-    "round" / > < rect
-x = "13"
-y = "13"
-width = "8"
-height = "8"
-fill = "#FFF"
-stroke = "#FFF"
-strokeWidth = {strokeWidth} strokeLinejoin = "round" / > < rect
-x = "27"
-y = "13"
-width = "8"
-height = "8"
-fill = "#FFF"
-stroke = "#FFF"
-strokeWidth = {strokeWidth} strokeLinejoin = "round" / > < rect
-x = "13"
-y = "27"
-width = "8"
-height = "8"
-fill = "#FFF"
-stroke = "#FFF"
-strokeWidth = {strokeWidth} strokeLinejoin = "round" / > < rect
-x = "27"
-y = "27"
-width = "8"
-height = "8"
-fill = "#FFF"
-stroke = "#FFF"
-strokeWidth = {strokeWidth} strokeLinejoin =
-    "round" / >
-    </>
+      x="6"
+      y="6"
+      width="36"
+      height="36"
+      rx="3"
+      fill={color}
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+    <rect
+      x="13"
+      y="13"
+      width="8"
+      height="8"
+      fill="#FFF"
+      stroke="#FFF"
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+    <rect
+      x="27"
+      y="13"
+      width="8"
+      height="8"
+      fill="#FFF"
+      stroke="#FFF"
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+    <rect
+      x="13"
+      y="27"
+      width="8"
+      height="8"
+      fill="#FFF"
+      stroke="#FFF"
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+    <rect
+      x="27"
+      y="27"
+      width="8"
+      height="8"
+      fill="#FFF"
+      stroke="#FFF"
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />
+  </>
 );
 
 const Light = ({ color, strokeWidth }) => (
@@ -56,28 +70,38 @@ const Light = ({ color, strokeWidth }) => (
       stroke={color}
       strokeWidth={strokeWidth}
       strokeLinejoin="round"
-    /><
-    rect
-x = "27"
-y = "13"
-width = "8"
-height = "8"
-fill = "none"
-stroke = {color} strokeWidth = {strokeWidth} strokeLinejoin = "round" / > < rect
-x = "13"
-y = "27"
-width = "8"
-height = "8"
-fill = "none"
-stroke = {color} strokeWidth = {strokeWidth} strokeLinejoin = "round" / > < rect
-x = "27"
-y = "27"
-width = "8"
-height = "8"
-fill = "none"
-stroke = {color} strokeWidth = {strokeWidth} strokeLinejoin =
-    "round" / > <
     />
+    <rect
+      x="27"
+      y="13"
+      width="8"
+      height="8"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+    <rect
+      x="13"
+      y="27"
+      width="8"
+      height="8"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+    <rect
+      x="27"
+      y="27"
+      width="8"
+      height="8"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+  </>
 );
 
 const ViewGrid = ({
@@ -99,56 +123,50 @@ const ViewGrid = ({
           xmlns="http:/ /
         www.w3.org / 2000 / svg "
           css={{
-  display: 'inline',
+            display: 'inline',
           }}
           {...props}
         >
-          <Light color={fill} strokeWidth={
-  1.5} />
+          <Light color={fill} strokeWidth={1.5} />
         </Svg>
       );
-          default:
+    default:
       return (
         <Svg
-      className = ""
-      width = {size || width || 24} height = {size || height || 24} viewBox =
-          "0 0 48 48"
-      xmlns = "http://www.w3.org/2000/svg"
+          className=""
+          width={size || width || 24}
+          height={size || height || 24}
+          viewBox="0 0 48 48"
+          xmlns="http://www.w3.org/2000/svg"
           css={{
-  display: 'inline',
+            display: 'inline',
           }}
           {...props}
         >
-          <Bold color={fill} strokeWidth={
-  1.5} />;
+          <Bold color={fill} strokeWidth={1.5} />;
         </Svg>
       );
-          }
-          }
-          ;
+  }
+};
+ViewGrid.displayName = 'ViewGrid';
 
-          ViewGrid.displayName = 'ViewGrid';
+Bold.propTypes = {
+  color: PropTypes.string,
+  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
-          Bold.propTypes = {
-            color : PropTypes.string,
-            strokeWidth :
-                PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
-          };
+Light.propTypes = {
+  color: PropTypes.string,
+  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
-          Light.propTypes = {
-            color : PropTypes.string,
-            strokeWidth :
-                PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
-          };
+ViewGrid.propTypes = {
+  fill: PropTypes.string,
+  filled: PropTypes.bool,
+  size: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
-          ViewGrid.propTypes = {
-            fill : PropTypes.string,
-            filled : PropTypes.bool,
-            size : PropTypes.number,
-            width : PropTypes.number,
-            height : PropTypes.number,
-            strokeWidth :
-                PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
-          };
-
-          export default ViewGrid;
+export default ViewGrid;
