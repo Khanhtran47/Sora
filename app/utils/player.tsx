1722a90b4681ae4ff9a317972176fa306599b367
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-12-15 15:04:03 +0530

    New Provider
    vidsrc & autoembed

--- app/utils/player.tsx
+++ app/utils/player.tsx
@@ -5,6 +5,10 @@ export default class Player {
 
   static readonly player3_base_url = 'databasegdriveplayer.co/player.php?';
 
+  static readonly player4_base_url = 'https://autoembed.to/';
+
+  static readonly player5_base_url = 'https://v2.vidsrc.me/embed/';
+
   static moviePlayerUrl = (id: number, player: number): string => {
     switch (player) {
       case 1:
@@ -13,6 +17,10 @@ export default class Player {
         return `${this.player2_base_url}imdb.php?id=${id}`;
       case 3:
         return `http://${this.player3_base_url}tmdb=${id}`;
+      case 4:
+        return `${this.player4_base_url}movie/tmdb/${id}?server=1&ss=4`;
+      case 5:
+        return `${this.player5_base_url}${id}`;
       default:
         return `${this.player1_base_url}movie?id=${id}`;
     }
@@ -26,6 +34,10 @@ export default class Player {
         return `${this.player2_base_url}tv-imdb.php?id=${id}&s=${season}`;
       case 3:
         return `http://series.${this.player3_base_url}type=series&tmdb=${id}&season=${season}&episode=${episode}`;
+      case 4:
+        return `${this.player4_base_url}tv/tmdb/${id}-${season}-${episode}?server=1&ss=4`;
+      case 5:
+        return `${this.player5_base_url}$id/${season}-${episode}`;
       default:
         return `${this.player1_base_url}tv?id=${id}&s=${season}&e=${episode}`;
     }
