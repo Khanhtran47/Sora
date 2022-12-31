064bff1481cbcf52503fae40054101d4790b3cac
Restyled.io <commits@restyled.io>
2022-12-31 02:55:18 +0000

    Restyled by clang-format

--- app/src/assets/icons/PauseIcon.js
+++ app/src/assets/icons/PauseIcon.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 const Bold = ({ color }) => (
   <g>
     <path
-      d="M10.65 19.11V4.89C10.65 3.54 10.08 3 8.64 3H5.01C3.57 3 3 3.54 3 4.89V19.11C3 20.46 3.57 21 5.01 21H8.64C10.08 21 10.65 20.46 10.65 19.11Z"
-      fill={color}
-    />
+d = "M10.65 19.11V4.89C10.65 3.54 10.08 3 8.64 3H5.01C3.57 3 3 3.54 3 4.89V19.11C3 20.46 3.57 21 5.01 21H8.64C10.08 21 10.65 20.46 10.65 19.11Z"
+fill =
+{
+  color
+} />
     <path
       d="M21.0016 19.11V4.89C21.0016 3.54 20.4316 3 18.9916 3H15.3616C13.9316 3 13.3516 3.54 13.3516 4.89V19.11C13.3516 20.46 13.9216 21 15.3616 21H18.9916C20.4316 21 21.0016 20.46 21.0016 19.11Z"
       fill={color}
-    />
-  </g>
+    / >
+    </g>
 );
 
 const Light = ({ color, strokeWidth }) => (
@@ -22,15 +24,13 @@ const Light = ({ color, strokeWidth }) => (
       strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
-    />
-    <path
-      d="M21 19.11V4.89C21 3.54 20.43 3 18.99 3H15.36C13.93 3 13.35 3.54 13.35 4.89V19.11C13.35 20.46 13.92 21 15.36 21H18.99C20.43 21 21 20.46 21 19.11Z"
-      stroke={color}
-      strokeWidth={strokeWidth}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </g>
+    /><
+    path
+d = "M21 19.11V4.89C21 3.54 20.43 3 18.99 3H15.36C13.93 3 13.35 3.54 13.35 4.89V19.11C13.35 20.46 13.92 21 15.36 21H18.99C20.43 21 21 20.46 21 19.11Z"
+stroke = {color} strokeWidth = {strokeWidth} strokeLinecap = "round"
+strokeLinejoin =
+    "round" / >
+    </g>
 );
 
 const Pause = ({
@@ -51,7 +51,7 @@ const Pause = ({
           {...props}
         >
           <Light color={fill} strokeWidth={1.5} />
-        </svg>
+    </svg>
       );
     default:
       return (
@@ -62,7 +62,8 @@ const Pause = ({
           {...props}
         >
           <Bold color={fill} />;
-        </svg>
+    <
+    /svg>
       );
   }
 };
