6915c351c532b4e7c12c1118702db5a995518968
Restyled.io <commits@restyled.io>
2023-01-01 18:26:44 +0000

    Restyled by prettier

--- app/src/assets/icons/FlipIcon.js
+++ app/src/assets/icons/FlipIcon.js
@@ -6,16 +6,27 @@ import Svg from '../../components/styles/Svg.styles';
 const Bold = ({ color }) => (
   <g>
     <path
-d = "M24 6V42"
-stroke = {color} strokeWidth = {1.5} strokeLinecap = "round"
-strokeLinejoin = "round" / > < path
-d = "M4 34L16 12V34H4Z"
-fill = {color} stroke = {color} strokeWidth = {1.5} strokeLinejoin =
-    "round" / > < path
-d = "M44 34H32V12L44 34Z"
-fill = {color} stroke = {color} strokeWidth = {1.5} strokeLinejoin =
-    "round" / >
-    </g>
+      d="M24 6V42"
+      stroke={color}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />{' '}
+    <path
+      d="M4 34L16 12V34H4Z"
+      fill={color}
+      stroke={color}
+      strokeWidth={1.5}
+      strokeLinejoin="round"
+    />{' '}
+    <path
+      d="M44 34H32V12L44 34Z"
+      fill={color}
+      stroke={color}
+      strokeWidth={1.5}
+      strokeLinejoin="round"
+    />
+  </g>
 );
 
 const Light = ({ color, strokeWidth }) => (
@@ -26,16 +37,22 @@ const Light = ({ color, strokeWidth }) => (
       strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
-    /><
-    path
-d = "M4 34L16 12V34H4Z"
-fill = "none"
-stroke = {color} strokeWidth = {strokeWidth} strokeLinejoin = "round" / > < path
-d = "M44 34H32V12L44 34Z"
-fill = "none"
-stroke = {color} strokeWidth = {strokeWidth} strokeLinejoin =
-    "round" / > <
-    /g>
+    />
+    <path
+      d="M4 34L16 12V34H4Z"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+    <path
+      d="M44 34H32V12L44 34Z"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinejoin="round"
+    />{' '}
+  </g>
 );
 
 const Flip = ({
@@ -57,54 +74,49 @@ const Flip = ({
           xmlns="http:/ /
         www.w3.org / 2000 / svg "
           css={{
-  display: 'inline',
+            display: 'inline',
           }}
           {...props}
         >
-          <Light color={fill} strokeWidth={
-  2} />
+          <Light color={fill} strokeWidth={2} />
         </Svg>
       );
-          default:
+    default:
       return (
         <Svg
-      className = ""
-      width = {size || width || 24} height = {size || height || 24} viewBox =
-          "0 0 48 48"
-      xmlns = "http://www.w3.org/2000/svg"
+          className=""
+          width={size || width || 24}
+          height={size || height || 24}
+          viewBox="0 0 48 48"
+          xmlns="http://www.w3.org/2000/svg"
           css={{
-  display: 'inline',
+            display: 'inline',
           }}
           {...props}
         >
-          <Bold color={
-  fill} />;
+          <Bold color={fill} />;
         </Svg>
       );
-          }
-          }
-          ;
+  }
+};
+Flip.displayName = 'FlipIcon';
 
-          Flip.displayName = 'FlipIcon';
+Bold.propTypes = {
+  color: PropTypes.string,
+};
 
-          Bold.propTypes = {
-            color : PropTypes.string,
-          };
+Light.propTypes = {
+  color: PropTypes.string,
+  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
-          Light.propTypes = {
-            color : PropTypes.string,
-            strokeWidth :
-                PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
-          };
+Flip.propTypes = {
+  fill: PropTypes.string,
+  filled: PropTypes.bool,
+  size: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
-          Flip.propTypes = {
-            fill : PropTypes.string,
-            filled : PropTypes.bool,
-            size : PropTypes.number,
-            width : PropTypes.number,
-            height : PropTypes.number,
-            strokeWidth :
-                PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
-          };
-
-          export default Flip;
+export default Flip;
--- app/src/assets/icons/RatioIcon.js
+++ app/src/assets/icons/RatioIcon.js
@@ -6,33 +6,43 @@ import Svg from '../../components/styles/Svg.styles';
 const Bold = ({ color }) => (
   <g>
     <rect
-x = "6"
-y = "6"
-width = "36"
-height = "36"
-rx = "3"
-fill = {color} stroke = {color} strokeWidth = "2"
-strokeLinejoin = "round" / > < path
-fillRule = "evenodd"
-clipRule = "evenodd"
-d = "M24 22.5C25.3807 22.5 26.5 21.3807 26.5 20C26.5 18.6193 25.3807 17.5 24 17.5C22.6193 17.5 21.5 18.6193 21.5 20C21.5 21.3807 22.6193 22.5 24 22.5Z"
-fill = "#FFF" / > < path
-fillRule = "evenodd"
-clipRule = "evenodd"
-d = "M24 30.5C25.3807 30.5 26.5 29.3807 26.5 28C26.5 26.6193 25.3807 25.5 24 25.5C22.6193 25.5 21.5 26.6193 21.5 28C21.5 29.3807 22.6193 30.5 24 30.5Z"
-fill = "#FFF" / > < path
-d = "M15.5 17V31"
-stroke = "#FFF"
-strokeWidth = "2"
-strokeLinecap = "round"
-strokeLinejoin = "round" / > < path
-d = "M32.5 17V31"
-stroke = "#FFF"
-strokeWidth = "2"
-strokeLinecap = "round"
-strokeLinejoin =
-    "round" / >
-    </g>
+      x="6"
+      y="6"
+      width="36"
+      height="36"
+      rx="3"
+      fill={color}
+      stroke={color}
+      strokeWidth="2"
+      strokeLinejoin="round"
+    />{' '}
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M24 22.5C25.3807 22.5 26.5 21.3807 26.5 20C26.5 18.6193 25.3807 17.5 24 17.5C22.6193 17.5 21.5 18.6193 21.5 20C21.5 21.3807 22.6193 22.5 24 22.5Z"
+      fill="#FFF"
+    />{' '}
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M24 30.5C25.3807 30.5 26.5 29.3807 26.5 28C26.5 26.6193 25.3807 25.5 24 25.5C22.6193 25.5 21.5 26.6193 21.5 28C21.5 29.3807 22.6193 30.5 24 30.5Z"
+      fill="#FFF"
+    />{' '}
+    <path
+      d="M15.5 17V31"
+      stroke="#FFF"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />{' '}
+    <path
+      d="M32.5 17V31"
+      stroke="#FFF"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </g>
 );
 
 const Light = ({ color, strokeWidth }) => (
@@ -47,30 +57,34 @@ const Light = ({ color, strokeWidth }) => (
       stroke={color}
       strokeWidth={strokeWidth}
       strokeLinejoin="round"
-    /><
-    path
-fillRule = "evenodd"
-clipRule = "evenodd"
-d = "M24 22.5C25.3807 22.5 26.5 21.3807 26.5 20C26.5 18.6193 25.3807 17.5 24 17.5C22.6193 17.5 21.5 18.6193 21.5 20C21.5 21.3807 22.6193 22.5 24 22.5Z"
-fill =
-{
-  color
-} />
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M24 22.5C25.3807 22.5 26.5 21.3807 26.5 20C26.5 18.6193 25.3807 17.5 24 17.5C22.6193 17.5 21.5 18.6193 21.5 20C21.5 21.3807 22.6193 22.5 24 22.5Z"
+      fill={color}
+    />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M24 30.5C25.3807 30.5 26.5 29.3807 26.5 28C26.5 26.6193 25.3807 25.5 24 25.5C22.6193 25.5 21.5 26.6193 21.5 28C21.5 29.3807 22.6193 30.5 24 30.5Z"
       fill={color}
-    / >
-    < path
-d = "M15.5 17V31"
-stroke = {color} strokeWidth = {strokeWidth} strokeLinecap = "round"
-strokeLinejoin = "round" / > < path
-d = "M32.5 17V31"
-stroke = {color} strokeWidth = {strokeWidth} strokeLinecap = "round"
-strokeLinejoin =
-    "round" / > <
-    /g>
+    />
+    <path
+      d="M15.5 17V31"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />{' '}
+    <path
+      d="M32.5 17V31"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />{' '}
+  </g>
 );
 
 const Ratio = ({
@@ -92,54 +106,49 @@ const Ratio = ({
           xmlns="http:/ /
         www.w3.org / 2000 / svg "
           css={{
-  display: 'inline',
+            display: 'inline',
           }}
           {...props}
         >
-          <Light color={fill} strokeWidth={
-  2} />
+          <Light color={fill} strokeWidth={2} />
         </Svg>
       );
-          default:
+    default:
       return (
         <Svg
-      className = ""
-      width = {size || width || 24} height = {size || height || 24} viewBox =
-          "0 0 48 48"
-      xmlns = "http://www.w3.org/2000/svg"
+          className=""
+          width={size || width || 24}
+          height={size || height || 24}
+          viewBox="0 0 48 48"
+          xmlns="http://www.w3.org/2000/svg"
           css={{
-  display: 'inline',
+            display: 'inline',
           }}
           {...props}
         >
-          <Bold color={
-  fill} />;
+          <Bold color={fill} />;
         </Svg>
       );
-          }
-          }
-          ;
+  }
+};
+Ratio.displayName = 'RatioIcon';
 
-          Ratio.displayName = 'RatioIcon';
+Bold.propTypes = {
+  color: PropTypes.string,
+};
 
-          Bold.propTypes = {
-            color : PropTypes.string,
-          };
+Light.propTypes = {
+  color: PropTypes.string,
+  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
-          Light.propTypes = {
-            color : PropTypes.string,
-            strokeWidth :
-                PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
-          };
+Ratio.propTypes = {
+  fill: PropTypes.string,
+  filled: PropTypes.bool,
+  size: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  strokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
 
-          Ratio.propTypes = {
-            fill : PropTypes.string,
-            filled : PropTypes.bool,
-            size : PropTypes.number,
-            width : PropTypes.number,
-            height : PropTypes.number,
-            strokeWidth :
-                PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
-          };
-
-          export default Ratio;
+export default Ratio;
