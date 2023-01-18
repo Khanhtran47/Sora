95fb83cdf9ff90f4d8ca27d1ea4396720f130716
Duc Khanh <duc_khanh.tran@insa-cvl.fr>
2023-01-18 05:58:31 +0100

    chore: update remix config

--- remix.config.js
+++ remix.config.js
@@ -4,14 +4,9 @@
 module.exports = {
   appDirectory: 'app',
   serverBuildTarget: 'vercel',
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-  serverBuildPath: 'api/index.js',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildDirectory: 'api/_build',
+  serverBuildDirectory: 'build',
   ignoredRouteFiles: ['.*'],
   serverDependenciesToBundle: [
     /^swiper.*/,
