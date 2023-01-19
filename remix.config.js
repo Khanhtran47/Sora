3dfa3fbb341eda9a035e423abbe60c9270bdaed7
Duc Khanh <duc_khanh.tran@insa-cvl.fr>
2023-01-19 18:39:43 +0100

    chore: revert remix config changes

--- remix.config.js
+++ remix.config.js
@@ -6,7 +6,10 @@ module.exports = {
   serverBuildTarget: 'vercel',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildDirectory: 'build',
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['.*'],
   serverDependenciesToBundle: [
     /^swiper.*/,
